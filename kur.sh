b8991325d3fe9b3cede3cb2ea8a178ed042ebe3a
Linux fan also loves Windows <63163893+Huso112@users.noreply.github.com>
2021-06-07 20:53:19 +0300

    Update kur.sh

--- kur.sh
+++ kur.sh
@@ -1,4 +1,4 @@
 mkdir ~/.dprmsrg
 cp -r ~/dprmsrg/erthqke.sh ~/.dprmsrg/
-echo " " >> ~/.bashrc && echo "#======deprem sorgulama======" >> ~/.bashrc && echo 'alias dprmsrg="sh ~/.dprmsrg/erthqke.sh"' >> ~/.bashrc && echo 'alias dprmsrg-help="xdg-open https://github.com/Huso112/dprmsrg/blob/main/README.md"
-echo "İşlem tamam. İsterseniz ev dizininizdeki dprmsrg dosyasını silebilirsiniz"
+echo " " >> ~/.bashrc && echo "#======deprem sorgulama======" >> ~/.bashrc && echo 'alias dprmsrg="sh ~/.dprmsrg/erthqke.sh"' >> ~/.bashrc && echo 'alias dprmsrg-help="xdg-open https://github.com/Huso112/dprmsrg/blob/main/README.md"' >> ~/.bashrc
+echo "İşlem tamam. İsterseniz ev dizininizdeki dprmsrg dosyasını silebilirsiniz" 
